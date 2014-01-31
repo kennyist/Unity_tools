32b74ce3cef8e34c66ee7837409e8a262747a550
Kennyist <cornishkid3@gmail.com>
2014-01-31 21:46:27 +0000

    Added saperated versions

---
+++ PlayerScripts/Interaction/Separated Version/InteractObject.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractObject : MonoBehaviour {
+
+    /*        
+        Interact Object
+        Author: Tristan 'Kennyist' Cunningham - www.tristanjc.com
+        Date: 31/01/2014
+        License: Creative Commons ShareAlike 3.0 - https://creativecommons.org/licenses/by-sa/3.0/
+    */
+
+
+    /* ------------------ In development ---------------- */
+    // Early version
+
+    public enum Type { Press, HoldForTime, HoldForTimeSlowReset }
+    public Type type;
+    public float TotalHoldTime = 3f;
+    public string InputButton = "f";
+    public float slowResetMultiplier = 0.1f;
+    private bool isHit;
+    private float holdTime;
+
+	void Start () {
+        holdTime = 0.0f;
+	}
+	
+	void Update () {
+        if ((type == Type.HoldForTime || type == Type.HoldForTimeSlowReset) && holdTime >= TotalHoldTime)
+        {
+            gameObject.SendMessage("InteractableComplete", SendMessageOptions.DontRequireReceiver);
+        }
+        else if (type == Type.Press && isHit && Input.GetKeyDown(InputButton))
+        {
+            gameObject.SendMessage("InteractableComplete", SendMessageOptions.DontRequireReceiver);
+        }
+
+        if (isHit && Input.GetKey(InputButton))
+        {
+            holdTime += Time.deltaTime;
+
+            if (holdTime > TotalHoldTime)
+            {
+                holdTime = TotalHoldTime;
+            }
+        }
+        else
+        {
+            if (type != Type.HoldForTimeSlowReset)
+            {
+                holdTime = 0.0f;
+            }
+            else
+            {
+                if (holdTime > 0.0f)
+                {
+                    holdTime -= Time.deltaTime * slowResetMultiplier;
+                }
+            }
+        }
+
+        Debug.Log(holdTime);
+	}
+
+    void InteractionHit(bool hit)
+    {
+        isHit = hit;
+    }
+
+    /// <summary>
+    /// Get the time the button has been held for.
+    /// </summary>
+    /// <returns>Time button has been held for while on this object</returns>
+    public float CurrentHeldTime()
+    {
+        return holdTime;
+    }
+
+    /// <summary>
+    /// Get the total hold time needed
+    /// </summary>
+    /// <returns>Total hold time needed</returns>
+    public float HoldTime()
+    {
+        return TotalHoldTime;
+    }
+
+    /// <summary>
+    /// Get the interaction type for this object
+    /// </summary>
+    /// <returns>Interaction type</returns>
+    public Type InteractionType()
+    {
+        return type;
+    }
+}
---
+++ PlayerScripts/Interaction/Separated Version/InteractPlayer.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractPlayer : MonoBehaviour {
+
+    /*        
+        Interact Player
+        Author: Tristan 'Kennyist' Cunningham - www.tristanjc.com
+        Date: 31/01/2014
+        License: Creative Commons ShareAlike 3.0 - https://creativecommons.org/licenses/by-sa/3.0/
+    */
+
+
+    /* ------------------ In development ---------------- */
+    // Early version
+
+    public LayerMask layerMask;
+    public Transform rayStartLocation;
+    public float castDistance = 1f;
+    private GameObject hitOBJ;
+    private GameObject lastOBJ;
+    private RaycastHit hit;
+    private float holdTime;
+
+	// Use this for initialization
+	void Start () {
+        hitOBJ = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(rayStartLocation.position, rayStartLocation.forward, out hit, castDistance, layerMask))
+        {
+            hitOBJ = hit.collider.gameObject;
+
+            if (lastOBJ != null)
+            {
+                if (hitOBJ != lastOBJ)
+                {
+                    Debug.Log("not equal");
+                    lastOBJ.SendMessage("InteractionHit", false, SendMessageOptions.DontRequireReceiver);
+                    lastOBJ = null;
+                }
+            }
+
+            if (hitOBJ.GetComponent<Interaction>() != null && hitOBJ != gameObject)
+            {
+                hitOBJ.SendMessage("InteractionHit", true, SendMessageOptions.DontRequireReceiver);
+            }
+            else
+            {
+                hitOBJ.SendMessage("InteractionHit", false, SendMessageOptions.DontRequireReceiver);
+                hitOBJ = null;
+            }
+        }
+        else
+        {
+            if (lastOBJ != null)
+            {
+                lastOBJ.SendMessage("InteractionHit", false, SendMessageOptions.DontRequireReceiver);
+                lastOBJ = null;
+            }
+        }
+
+        if (hitOBJ != null)
+        {
+            lastOBJ = hitOBJ;
+        }
+	}
+}
